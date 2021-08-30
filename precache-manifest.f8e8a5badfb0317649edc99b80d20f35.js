0f7c3e49105fb6e1d3d04efece5d4ddddba301d3
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-30 13:16:43 +0300

    Updates

---
+++ precache-manifest.f8e8a5badfb0317649edc99b80d20f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8de56a0be6a08589ddcd844a821960",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c5c8efaec2ef677945b",
+    "url": "./static/css/main.3d13edea.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5c8efaec2ef677945b",
+    "url": "./static/js/main.86445e8d.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
